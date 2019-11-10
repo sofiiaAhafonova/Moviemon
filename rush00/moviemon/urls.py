c9b4820b8d2ec65cc537f326a72d2ecea6c4c49f
sofiiaAhafonova <amita051197@gmail.com>
2019-11-10 12:18:10 +0200

    Add urls

--- rush00/moviemon/urls.py
+++ rush00/moviemon/urls.py
@@ -5,4 +5,10 @@ from moviemon import views
 urlpatterns = [
     re_path(r'^$', views.home, name='home'),
     re_path(r'^worldmap/$', views.worldmap, name='worldmap'),
+    re_path(r'^battle/<moviemon_id>/$', views.battle, name='battle' ),
+    re_path(r'^moviedex/$', views.moviedex, name='moviedex'),
+    re_path(r'^moviedex/<moviemon_id>/$', views.details, name='details'),
+    re_path(r'^options/$', views.options, name='options'),
+    re_path(r'^options/save_game/$', views.save_game, name='save_game'),
+    re_path(r'^options/load_game/$', views.load_game, name='load_game'),
 ]
--- rush00/moviemon/views.py
+++ rush00/moviemon/views.py
@@ -14,3 +14,42 @@ def worldmap(request):
         'title': 'Worldmap Page Title'
         }
     )
+
+def battle(request):
+    return render(request, 'home.html', {
+        'title': 'Battle Page Title'
+        }
+    )
+
+
+def moviedex(request):
+    return render(request, 'home.html', {
+        'title': 'Moviedex Page Title'
+        }
+    )
+
+
+def details(request):
+    return render(request, 'home.html', {
+        'title': 'Details Page Title'
+        }
+    )
+
+def options(request):
+    return render(request, 'home.html', {
+        'title': 'Options Page Title'
+        }
+    )
+
+
+def save_game(request):
+    return render(request, 'home.html', {
+        'title': 'Save Game Page Title'
+        }
+    )
+
+def load_game(request):
+    return render(request, 'home.html', {
+        'title': 'Load Game Page Title'
+        }
+    )
