7e189c65f3942d83aa7373391f2185933a666151
sofiiaAhafonova <amita051197@gmail.com>
2019-11-10 12:54:34 +0200

    Create html pages

--- rush00/moviemon/views.py
+++ rush00/moviemon/views.py
@@ -10,46 +10,46 @@ def home(request):
     )
 
 def worldmap(request):
-    return render(request, 'home.html', {
+    return render(request, 'worldmap.html', {
         'title': 'Worldmap Page Title'
         }
     )
 
 def battle(request):
-    return render(request, 'home.html', {
+    return render(request, 'battle.html', {
         'title': 'Battle Page Title'
         }
     )
 
 
 def moviedex(request):
-    return render(request, 'home.html', {
+    return render(request, 'moviedex.html', {
         'title': 'Moviedex Page Title'
         }
     )
 
 
 def details(request):
-    return render(request, 'home.html', {
+    return render(request, 'details.html', {
         'title': 'Details Page Title'
         }
     )
 
 def options(request):
-    return render(request, 'home.html', {
+    return render(request, 'options.html', {
         'title': 'Options Page Title'
         }
     )
 
 
 def save_game(request):
-    return render(request, 'home.html', {
+    return render(request, 'save_game.html', {
         'title': 'Save Game Page Title'
         }
     )
 
 def load_game(request):
-    return render(request, 'home.html', {
+    return render(request, 'load_game.html', {
         'title': 'Load Game Page Title'
         }
     )
